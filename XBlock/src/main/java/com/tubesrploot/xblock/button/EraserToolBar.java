72906ebad2874b04e73beca3c8213a7641c11567
farizko <mfarizko@gmail.com>
2014-05-19 13:16:35 +0700

    Update EraserToolBar.java
    command

--- XBlock/src/main/java/com/tubesrploot/xblock/button/EraserToolBar.java
+++ XBlock/src/main/java/com/tubesrploot/xblock/button/EraserToolBar.java
@@ -20,16 +20,16 @@ public class EraserToolBar extends JToolBar {
         setOrientation(JToolBar.VERTICAL);
         setToolTipText("eraser toolbar");
     }
-    
+    //pengaturan ukuran button
     public void initDimension(){
         Dimension dimension = new Dimension(nWidth, nHeight);
         setPreferredSize(dimension);
     }
-    
+    //pengaturan high button
     public void setNHeight(int nHeight) {
         this.nHeight = nHeight;
     }
-    
+    //pengaturan width button
     public void setNWidth(int nWidth) {
         this.nWidth = nWidth;
     }
